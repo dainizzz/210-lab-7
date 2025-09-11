813fd451515f9b53e4d353da4c31e2a624022cc5
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-10 23:37:32 -0700

    Initial commit

---
+++ 210-lab-7.cpp
@@ -0,0 +1,20 @@
+// COMSC-210 | Lab 7 | Dainiz Almazan
+// IDE used: CLion
+// TODO: pointer notation rather than array notation
+#include <string>;
+using namespace std;
+
+// reverseArray() receives a dynamic string array, reverses the elements, and returns a pointer to the modified array.
+// arguments: a dynamic array of type string
+// returns: a pointer to the reversed array
+string* reverseArray();
+// displayArray() receives a dynamic string array, and outputs its elements.
+// arguments: a dynamic array of type string
+// returns: nothing
+void displayArray();
+
+int main(){
+// Create a dynamic string array and populate it with 5 names of your choice.
+// Call the two functions appropriately to reverse and output the array.
+  return 0;
+}
